3240bc6f030e7df2b0e24182153528e978cf0451
lykov.a <a.lykov921@gmail.com>
2016-08-22 22:39:56 +0300

    done node.js

--- src/node.js
+++ src/node.js
@@ -8,36 +8,85 @@ class Node {
 	}
 
 	appendChild(node) {
-			if(this.left == null){
-				this.left = node
+			if(!this.left){
+				this.left = node;
+				node.parent = this;
 			}
-			else{
-				this.right = node
+			else if(!this.right){
+				this.right = node;
+				node.parent = this;
 			}
-		//TODO if left & right exist
 	}
 
 	removeChild(node) {
 		if(node == this.left){
-			this.left = null
+			this.left = null;
+			node.parent = null;
 		}
 		else if(node == this.right){
-			this.right = null
+			this.right = null;
+			node.parent = null
 		}
-		else if(node != this.right && node != this.left){
+		else{
 			throw new Error();
 		}
 	}
 
 	remove() {
-		if(this.parent != null){
-			//TODO
+		if(this.parent){
+			this.parent.removeChild(this)	
 		}
 	}
 
 	swapWithParent() {
-		if(this.parent != null){
-			//TODO
+		if(this.parent){
+			let node = this,
+				parent = this.parent,
+				leftChild = this.left,
+				rightChild = this.right,
+				parentOfParent = this.parent.parent,
+				leftChildOfParent = this.parent.left,
+				rightChildOfParent = this.parent.right;
+
+			//обновляем с потомков node и с родителя node на потомков
+			if(leftChild){
+				leftChild.parent = parent;
+				parent.left = leftChild;
+			}
+			if(rightChild){
+				rightChild.parent = parent;
+				parent.right = rightChild;
+			}
+
+			//...и, если необходимо, соседнюю ветку
+			if(leftChildOfParent == node){
+				node.left = parent;
+				parent.parent = node;
+				if(rightChildOfParent){
+					rightChildOfParent.parent = node
+					node.right = rightChildOfParent;
+				}
+			}
+			else if(rightChildOfParent == node){
+				node.right = parent;
+				parent.parent = node;
+				if(leftChildOfParent){
+					leftChildOfParent.parent = node
+					node.left = leftChildOfParent;
+				}
+			}
+
+			//обновляем с родителя родителя node
+			if(parentOfParent){
+				this.parent = parentOfParent;
+				this.parent.parent = this;
+				if(parentOfParent.left == parent){
+					parentOfParent.left = node
+				}
+				else if(parentOfParent.right == parent){
+					parentOfParent.right = node
+				}
+			}
 		}
 	}
 }
